a85875ab6df32d281d3514eaadb5f552f94ebb20
Arthur de Sá Braz de Matos <129903540+arthursbmatos9@users.noreply.github.com>
2023-11-24 00:21:24 -0300

    Update pag_inicial.js

--- Codigo/frontEnd/javascript/pag_inicial.js
+++ Codigo/frontEnd/javascript/pag_inicial.js
@@ -1,22 +1,22 @@
 //REDIRECIONAMENTO PARA CLICKBUS
 function removeAccents(str) {
-            return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-        }
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
 
-        document.getElementById("buscarRotas").addEventListener("click", function() {
-            var origin = removeAccents(document.getElementById("origin").value.toLowerCase().replace(/ /g, '-'));
-            var destination = removeAccents(document.getElementById("destination").value.toLowerCase().replace(/ /g, '-'));
-            var departureDate = document.getElementById("departureDate").value;
-            var returnDate = document.getElementById("returnDate").value;
-            
-            // Verifica se a cidade de destino é "São Paulo - SP" e adiciona "todos" se necessário.
-            if (destination === "sao-paulo-sp") {
-                destination = "sao-paulo-sp-todos";
-            }
-            
-            var searchUrl = "https://www.clickbus.com.br/onibus/" + origin + "/" + destination + "?departureDate=" + departureDate + "&returnDate=" + returnDate;
-            window.location.href = searchUrl;
-        });
+document.getElementById("buscarRotas").addEventListener("click", function() {
+  var origin = removeAccents(document.getElementById("origin").value.toLowerCase().replace(/ /g, '-'));
+  var destination = removeAccents(document.getElementById("destination").value.toLowerCase().replace(/ /g, '-'));
+  var departureDate = document.getElementById("departureDate").value;
+  var returnDate = document.getElementById("returnDate").value;
+
+  // Verifica se a cidade de destino é "São Paulo - SP" e adiciona "todos" se necessário.
+  if (destination === "sao-paulo-sp") {
+    destination = "sao-paulo-sp-todos";
+  }
+
+  var searchUrl = "https://www.clickbus.com.br/onibus/" + origin + "/" + destination + "?departureDate=" + departureDate + "&returnDate=" + returnDate;
+  window.location.href = searchUrl;
+});
 
 //BTN LOGIN
 const btnLogin = document.getElementById("login");
@@ -60,6 +60,16 @@ const btnMenu = document.getElementById("menu_opcoes");
 btnMenu.addEventListener('click', function() {
 
   window.location.href = 'index.html';
+
+});
+
+
+//BTN CHATBOT
+const btnChatBot = document.getElementById("btnChatBot");
+
+btnChatBot.addEventListener('click', function() {
+
+  window.location.href = 'bot.html';
 
 });
 
