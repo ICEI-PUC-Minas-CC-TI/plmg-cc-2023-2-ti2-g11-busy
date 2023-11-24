122f90e57305a68773e5f2788b2840a140e8fe28
Arthur de Sá Braz de Matos <129903540+arthursbmatos9@users.noreply.github.com>
2023-11-24 00:08:15 -0300

    Update Aplicacao.java

--- Codigo/backEnd/src/app/Aplicacao.java
+++ Codigo/backEnd/src/app/Aplicacao.java
@@ -19,19 +19,21 @@ public class Aplicacao {
         
         post("/cadastrarCliente", (request, response) -> clienteService.insert(request, response));
         get("/cliente/:id", (request, response) -> clienteService.get(request, response));
+        post("/cliente/:email", (request, response) -> clienteService.getByEmail(request, response));
         post("/cliente/update/:id", (request, response) -> clienteService.update(request, response));
-        get("/cliente/delete/:id", (request, response) -> clienteService.delete(request, response));
+        delete("/cliente/delete/:id", (request, response) -> clienteService.delete(request, response));
 
         
         post("/cadastrarViagem", (request, response) -> viagemService.insert(request, response));
         get("/viagem/:id", (request, response) -> viagemService.get(request, response));
         post("/viagem/update/:id", (request, response) -> viagemService.update(request, response));
-        get("/viagem/delete/:id", (request, response) -> viagemService.delete(request, response));
+        delete("/viagem/delete/:id", (request, response) -> viagemService.delete(request, response));
         
         
         post("/cadastrarPagamento", (request, response) -> pagamentoService.insert(request, response));
         get("/pagamento/:id", (request, response) -> pagamentoService.get(request, response));
         post("/pagamento/update/:id", (request, response) -> pagamentoService.update(request, response));
-        get("/pagamento/delete/:id", (request, response) -> pagamentoService.delete(request, response));
+        delete("/pagamento/delete/:id", (request, response) -> pagamentoService.delete(request, response));
     }
 }
+
